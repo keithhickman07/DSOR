441213f06fe546fcc1f1ef7723e442ee0710c0a4
Keith Hickman <keonhick@iu.edu>
2018-02-19 05:59:38 -0500

    completed asst4

--- Assignment4.py
+++ Assignment4.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 from numpy.linalg import inv
 from sklearn import linear_model
+from sklearn.feature_selection import RFE, RFECV
+from sklearn.linear_model import LinearRegression
+from time import time
 
 # Problem 1
 
@@ -71,9 +74,8 @@ calAccuracy(testLabel, predictedLabel)
 # Problem 2
 
 data = pd.read_csv("~/PycharmProjects/DSOR/clones/DSOR/Resources/iris.csv")
-print(data.head())
 data['class'] = data['class'].replace(['Iris-setosa', 'Iris-virginica', 'Iris-versicolor'], ['1', '0', '0'])
-
+print(data.head())
 
 def zScoreNorm(targetCol, data):
     for i in data.columns:
@@ -95,8 +97,52 @@ def dataFilter(targetColumn, data):
     return trainingData, np.asarray(trainingLabel).flatten(), \
            testData, np.asarray(testLabel).flatten()
 
+def calSSE(target, predicted):
+    m = len(target)
+    SSE = (np.asarray(target) - np.asarray(predicted) ** 2) / float(2*m)
+    return SSE
 
-# dataFilter('class', data) examining output
+def predict(data, target, test=[]):
+    model = LinearRegression(fit_intercept=True, normalize=True, n_jobs=-1)
+    model.fit(data, target)
+    if len(test) == 0:
+        return model.predict(data)
+    return model.predict(test)
 
-# Problem 2 - 5 best features.
+dataFrame, labelFrame = dataFilter('class', data)
 
+model = LinearRegression(fit_intercept=True, normalize=True, n_jobs=-1)
+
+rfe = RFE(model, 4)
+
+rfe = rfe.fit(dataFrame, labelFrame)
+
+print(rfe.result.ranking_)
+
+print(np.asarray(rfe.result.support_)) # changed from dataFrame[result_support)
+
+start = time.clock()
+
+trainData, trainLabel, testData, testLabel = dataFilter(data, 'class')
+
+predictedLabel = predict(trainData, trainLabel, testData)
+
+end = time.clock()
+
+print ("Total time taken with all variables ", end - start)
+
+print ("SSE with all variables :", calSSE(testLabel, predictedLabel))
+
+# then do linear regression using only selected features
+
+start = time.clock()
+
+trainData, trainLabel, testData, testLabel = (dataFilter(data, 'class'))
+
+predictedLabel = predict(trainData, trainLabel, testData)
+
+end = time.clock()
+
+print ("Total time - all variables ", end - start)
+
+print ("SSE with selected variables :", calSSE(testLabel, predictedLabel))
